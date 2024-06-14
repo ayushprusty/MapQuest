1952f59d170bf3628361a168058444394bec164d
AYUSH PRUSTY <ayush20427@iiitd.ac.in>
2024-06-14 14:32:11 +0530

    Initial Commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+const fs = require('fs');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+const PORT = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+let locationData = [];
+
+fs.readFile('vehicle_location.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error(err);
+        return;
+    }
+    locationData = JSON.parse(data);
+});
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    let index = 0;
+
+    const sendLocationData = () => {
+        if (index < locationData.length) {
+            const location = locationData[index];
+            const coordinates = location.loc.coordinates;
+            const heading = location.hd;
+
+            socket.emit('location_update', {
+                latitude: coordinates[0],
+                longitude: coordinates[1],
+                heading: heading
+            });
+            index++;
+            setTimeout(sendLocationData, 70); // Adjust the interval as needed
+        }
+    };
+
+    sendLocationData();
+});
+
+server.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
